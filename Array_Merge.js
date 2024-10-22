675ee152a2c4a52613ab1eb011b926a9f57e1b5c
Tu-Amigo0 <obedg549@gmail.com>
2024-10-21 20:57:41 -0600

    Automatizacion_Pruebas

---
+++ Array_Merge.js
@@ -0,0 +1,11 @@
+/*declaras un arreglo merge*/
+let arreglo1 = [1, 2, 3];
+let arreglo2 = [4, 5, 6];
+
+// Fusionar los dos arreglos
+let arregloMerge = arreglo1.concat(arreglo2);
+
+console.log(arregloMerge); // Imprime [1, 2, 3, 4, 5, 6]
+
+
+
